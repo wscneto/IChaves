59d29523ce6df888e1aedd5bd7cbe0b6ad5c6054
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-26 14:39:16 -0300

    feat: initial structure

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
