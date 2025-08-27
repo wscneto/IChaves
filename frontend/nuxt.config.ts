f4aaa2332aca0adc0b137808dc6903ee16c64947
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-27 17:00:29 -0300

    feat: import libraries

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/icon'],
+  css: ['~/assets/css/main.css', '~/assets/css/fonts.css', '~/assets/css/colors.css'],
   vite: {
     plugins: [
       tailwindcss(),
