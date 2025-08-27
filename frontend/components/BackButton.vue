75991fd96617b06d170dfc0d40a229f78a469815
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-27 18:36:32 -0300

    feat: create BackButton component

---
+++ frontend/components/BackButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button
+      @click="$router.back()"
+      class="mb-4 px-4 py-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer flex items-center"
+    >
+      <Icon name="bx:arrow-back" class="mr-1" />
+      Voltar
+    </button>
+  </div>
+</template>
