e5ddf30c1582a8a500f9922e0b2d8efdde088b5e
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-29 11:51:49 -0300

    feat: show different header based on user type (André)

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <AppHeader />
+    <component :is="auth.user.role === 'admin' ? AppHeaderAdmin : AppHeaderStudent" />
     <div class="pt-[60px] sm:pt-[80px] lg:pt-[100px]">
       <slot />
     </div>
     <AppFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import AppHeaderAdmin from '@/components/AppHeaderAdmin.vue'
+import AppHeaderStudent from '@/components/AppHeaderStudent.vue'
+import AppFooter from '@/components/AppFooter.vue'
+
+const auth = useAuthStore()
+</script>
