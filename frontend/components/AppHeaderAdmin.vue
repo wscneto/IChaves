49b4af815eae7aac6e58a71b78a5394e8d482599
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-29 11:49:30 -0300

    feat: separate header into student header and admin header

---
+++ frontend/components/AppHeaderAdmin.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="fixed w-full h-[60px] sm:h-[80px] lg:h-[100px] flex items-center justify-between px-4 py-3 bg-gray-100 shadow-md z-100">
+    <NuxtLink to="/">
+      <h1 class="text-2xl font-bold text-red-600"><span class="text-blue-600">IC</span>haves Admin</h1>
+    </NuxtLink>
+    <div class="flex items-center space-x-3">
+
+      <NotificationBell />
+
+      <NuxtLink to="/user" class="flex items-center space-x-3">
+        <span class="hidden sm:block text-gray-700 font-medium text-xl ">
+          Walter Neto
+        </span>
+        <img
+          src="https://ui-avatars.com/api/?name=Walter+Neto"
+          alt="User avatar"
+          class="w-10 h-10 rounded-full border border-gray-300"
+        />
+      </NuxtLink>
+    </div>
+  </header>
+</template>
