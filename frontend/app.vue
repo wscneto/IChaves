d1870736e351cacf03a63346a18faf5747c335bc
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-31 12:59:28 -0300

    feat: add fade transition when user navigates through pages

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,27 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <NuxtPage
+      :transition="{
+        name: 'page',
+        mode: 'out-in'
+      }"
+    />
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s ease;
+}
+
+.page-enter-from {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.page-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+</style>
