0bab0e0eaff3f35c40334a380ccf481ed4eb6846
Walter Soares Costa Neto <contato.walter@proton.me>
2025-09-02 09:49:02 -0300

    feat: add top bar showing current user.role and button to change role

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,6 +2,12 @@
   <div>
     <component :is="auth.user.role === 'admin' ? AppHeaderAdmin : AppHeaderStudent" />
     <div class="pt-[60px] sm:pt-[80px] lg:pt-[100px]">
+      <div class="p-2 sm:p-4 bg-blue-600 text-white text-center">
+        <p class="text-sm sm:text-base md:text-lg font-normal">
+          Esta é a página {{ auth.user.role === 'admin' ? 'da secretaria' : 'do estudante' }}. Para acessar a página 
+          {{ auth.user.role === 'admin' ? 'do estudante' : 'da secretaria' }}, clique <span class="cursor-pointer text-yellow-400 hover:text-green-500 font-bold" @click="toggleRole">aqui.</span>
+        </p>
+      </div>
       <slot />
     </div>
     <AppFooter />
@@ -15,4 +21,8 @@ import AppHeaderStudent from '@/components/AppHeaderStudent.vue'
 import AppFooter from '@/components/AppFooter.vue'
 
 const auth = useAuthStore()
+
+function toggleRole() {
+  auth.setRole(auth.user.role === 'admin' ? 'student' : 'admin')
+}
 </script>
