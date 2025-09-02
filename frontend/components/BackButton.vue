caa5cc5a6d9d67550e0d1ee1541e778802dfe7d7
Walter Soares Costa Neto <contato.walter@proton.me>
2025-09-02 09:51:56 -0300

    refactor: make BackButton navigate to home page instead of going back 1 page in the history

--- frontend/components/BackButton.vue
+++ frontend/components/BackButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button
-      @click="$router.back()"
+      @click="goHome"
       class="mb-4 px-4 py-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer flex items-center"
     >
       <Icon name="bx:arrow-back" class="mr-1" />
@@ -9,3 +9,9 @@
     </button>
   </div>
 </template>
+
+<script setup lang="ts">
+  function goHome() {
+    navigateTo('/')
+  }
+</script>
